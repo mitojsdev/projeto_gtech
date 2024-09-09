d3c3695740233436ac4c67855eab0b28c07fe72f
Amilton Junio <amiltonsogm@gmail.com>
2024-09-09 00:03:43 -0300

    ajuste no arquivo de migração para correto funcionamento

--- migração.py
+++ migração.py
@@ -6,10 +6,11 @@ sqlite_conn = sqlite3.connect('./Banco/BancoGtech.db')
 sqlite_cur = sqlite_conn.cursor()
 
 # Conexão com o banco de dados PostgreSQL
+#conn = psycopg2.connect(database="localhost", user="root", password="root")
 conn = psycopg2.connect(
     host="localhost",
-    database="PostgreSQL16",
-    user="root",
+    database="postgres",
+    user="postgres",
     password="root",
     
 )
@@ -18,17 +19,19 @@ cursor = conn.cursor()
 
 
 # Seleciona todos os dados da tabela no SQLite
-sqlite_cur.execute("SELECT * FROM TB_CLIENTE")
+sqlite_cur.execute("SELECT * FROM TB_VENDA")
 dados_sqlite = sqlite_cur.fetchall()
 print(dados_sqlite)
 
-
+#CREATE TABLE "TB_VENDA" ( ID INT NOT NULL, DATA DATE NOT NULL, ID_CLIENTE INT, ID_PRODUTO INT, QUANTIDADE INT, PRECO_VENDA FLOAT NOT NULL, LUCRO FLOAT, PRIMARY KEY (ID), FOREIGN KEY(ID_CLIENTE) REFERENCES TB_CLIENTE (ID_CLIENTE), FOREIGN KEY(ID_PRODUTO) REFERENCES TB_PRODUTO_NEW (ID_PRODUTO) )
 for row in dados_sqlite:
     cursor.execute("""
-        INSERT INTO TB_CLIENTE (ID_CLIENTE, NOME, TELEFONE, DATA_CADASTRO)
-        VALUES (%s, %s, %s, %s)
+        INSERT INTO "TB_VENDA" ("ID", "DATA", "ID_CLIENTE","ID_PRODUTO", "QUANTIDADE", "PRECO_VENDA", "LUCRO")
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
     """, row)
+    
 
 # Comitar as alterações no PostgreSQL
-cursor.commit()
+conn.commit()
+
 
