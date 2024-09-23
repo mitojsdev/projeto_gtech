71708da36545897ed6e8106ce0d718dcfaba96ed
Amilton Junio <amiltonsogm@gmail.com>
2024-09-22 22:20:27 -0300

    grafico alterado para lucro mensal

--- graficos.py
+++ graficos.py
@@ -3,41 +3,56 @@ from datetime import datetime
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
 
-def buscar_vendas_mes_atual():
+def buscar_lucro_por_mes():
 
     conexao = conectar()
     cursor= conexao.cursor()
 
-    mes_atual = datetime.now().month
-    ano_atual = datetime.now().year
+   
 
+    #cursor que busca venda por mês
+    # cursor.execute('''
+    #         SELECT EXTRACT(DAY FROM "DATA")::int, COUNT(*) 
+    #         FROM "TB_VENDA"
+    #         WHERE EXTRACT(MONTH FROM "DATA") = %s AND EXTRACT(YEAR FROM "DATA") = %s
+    #         GROUP BY EXTRACT(DAY FROM "DATA")
+    #         ORDER BY EXTRACT(DAY FROM "DATA")
+    #     ''', (mes_atual, ano_atual))
+    
+    #consulta de lucro por mês
     cursor.execute('''
-            SELECT EXTRACT(DAY FROM "DATA")::int, COUNT(*) 
-            FROM "TB_VENDA"
-            WHERE EXTRACT(MONTH FROM "DATA") = %s AND EXTRACT(YEAR FROM "DATA") = %s
-            GROUP BY EXTRACT(DAY FROM "DATA")
-            ORDER BY EXTRACT(DAY FROM "DATA")
-        ''', (mes_atual, ano_atual))
+    SELECT 
+        EXTRACT(YEAR FROM "DATA") AS ano,
+        EXTRACT(MONTH FROM "DATA") AS mes,
+        SUM("LUCRO") AS lucro_mensal
+    FROM 
+    "TB_VENDA"
+    GROUP BY 
+        EXTRACT(YEAR FROM "DATA"), EXTRACT(MONTH FROM "DATA")
+    ORDER BY 
+        ano, mes;''')
 
-    vendas_dia = cursor.fetchall()
+
+    lucro = cursor.fetchall()
     conexao.close
-    return vendas_dia
+    return lucro
 
 def criar_grafico(root):
-    vendas = buscar_vendas_mes_atual()
-    dias = [venda[0] for venda in vendas]
-    qtd_vendas = [venda[1] for venda in vendas]
+    vendas = buscar_lucro_por_mes()
+    meses = [f'{venda[0]}/{venda[1]}' for venda in vendas]  # Combinação de ano/mês
+    lucros = [venda[2] for venda in vendas]
 
     fig, ax = plt.subplots()
-    ax.bar(dias, qtd_vendas, color='blue')
-    
-    ax.set_title(f'Vendas no Mês {datetime.now().strftime("%B")}')
-    ax.set_xlabel('Dia')
-    ax.set_ylabel('Quantidade de Vendas')
+    ax.bar(meses, lucros, color='green')
 
-    ax.set_xticks(dias)
-    ax.set_yticks(qtd_vendas)
-    
+    ax.set_title('Lucro por Mês')
+    ax.set_xlabel('Mês/Ano')
+    ax.set_ylabel('Lucro Total')
+
+    # Ajustar os ticks para os meses
+    ax.set_xticks(range(len(meses)))
+    ax.set_xticklabels(meses, rotation=45)  # Rotacionar os labels para melhor visualização
+
     # Exibir o gráfico no tkinter
     canvas = FigureCanvasTkAgg(fig, master=root)
     canvas.draw()
